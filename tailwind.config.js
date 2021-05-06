b29f4658f48973214034439b6a9b16ea9c329d7b
shfshanyue <xianger94@gmail.com>
2021-05-06 11:19:11 +0800

    chore: update shanchui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        orange: colors.orange
+      },
       minHeight: theme => theme('spacing'),
       minWidth: theme => theme('spacing'),
       screens: {
